01d3a587bf817878c80ac06332c468aee154d796
dit02 <dit@imac-7.local>
2019-05-02 12:59:03 +0900

    테이블뷰 테스트

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let row = indexPath.row
+        let sec = indexPath.section
+        
+        cell.textLabel?.text = "Section: " + String(sec)
+        cell.detailTextLabel?.text = "Row" + String(row)
+        
+        print("cell....")
+        
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        return 100.0
     }
 
-
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("Section = \(indexPath.section)" + "Row = \(indexPath.row)")
+    }
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
 }
 
